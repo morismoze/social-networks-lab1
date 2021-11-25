9adfe8def5df70b48cc9feaa110d840c4c24ad7b
Moris Moze <moris.moze97@gmail.com>
2021-11-25 22:29:57 +0100

    Revert tempory attempt at fixing firefox redirect problem

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -24,7 +24,7 @@ const LoginPage = () => {
 
                 if (searchParams.state) {
                     getUserFbData(() => {
-                        navigate('/', { replace: true });
+                        navigate(decodeURIComponent(searchParams.state), { replace: true });
                     });
                 } else {
                     getUserFbData(() => {
