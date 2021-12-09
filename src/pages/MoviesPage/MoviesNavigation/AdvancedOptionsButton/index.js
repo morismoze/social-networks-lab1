51cb503a2bdd7c8ac58a3e6a146ad8fda9faa1b3
Moris Moze <moris.moze97@gmail.com>
2021-12-09 21:05:55 +0100

    Refactor filter styling on mobile

--- src/pages/MoviesPage/MoviesNavigation/AdvancedOptionsButton/index.js
+++ src/pages/MoviesPage/MoviesNavigation/AdvancedOptionsButton/index.js
@@ -3,11 +3,27 @@ import React from 'react';
 import { AiOutlineExpandAlt, AiOutlineShrink } from "react-icons/all";
 
 import styles from './AdvancedOptionsButton.module.scss';
+import {useWindowSize} from "../../../../hooks/useWindowSize";
 
 const AdvancedOptionsButton = ({
     isOpen,
     onClick
 }) => {
+    const { width } = useWindowSize();
+
+    if (width <= 576) {
+        return (
+            <div
+                onClick={onClick}
+                className={styles.mobileAdvancedOptionBtn}
+            >
+                <span className={styles.mobileAdvancedOptionBtn__text}>
+                    {!isOpen ? 'Show filter options' : 'Hide filter options'}
+                </span>
+            </div>
+        );
+    }
+
     return (
         <>
             {isOpen ? (
