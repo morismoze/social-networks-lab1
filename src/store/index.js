7737f7d2b1487a5f0411c53ec5ad7848cd92d72d
Moris Moze <moris.moze97@gmail.com>
2021-12-10 14:42:31 +0100

    Disable redux store devtools on prod

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { rootReducer } from './reducer';
 
+const environment = process.env.REACT_APP_NODE_ENV;
+
 const store = configureStore({
     reducer: rootReducer,
+    devTools: environment === 'dev'
 });
 
 export default store;
