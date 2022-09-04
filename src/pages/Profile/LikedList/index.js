d034eea2649f769d4b74b14cf082be8479796fd8
morismoze <moris.moze97@gmail.com>
2022-09-04 16:03:13 +0200

    Fix styling

--- src/pages/Profile/LikedList/index.js
+++ src/pages/Profile/LikedList/index.js
@@ -1,54 +1,48 @@
 import React from 'react';
 
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { Helmet } from "react-helmet";
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Helmet } from 'react-helmet';
 
-import Title from "../MoviesTable/Title";
-import MoviesTable from "../MoviesTable";
-import MoviesMobileList from "../MoviesMobileList";
+import Title from '../MoviesTable/Title';
+import MoviesTable from '../MoviesTable';
+import MoviesMobileList from '../MoviesMobileList';
 import * as UserSelectors from '../../../store/shared/user/User.selectors';
 import styles from './LikedList.module.scss';
 
 const LikedList = () => {
-    const userName = useSelector(UserSelectors.name);
-    const likedMovies = useSelector(UserSelectors.likedMovies);
+  const userName = useSelector(UserSelectors.name);
+  const likedMovies = useSelector(UserSelectors.likedMovies);
 
-    return (
-        <>
-            <Helmet>
-                <title>{userName} - Liked list &bull; Recommend.me</title>
-            </Helmet>
-            <div className={styles.likedList}>
-                <Title title='Movies You liked'/>
-                {likedMovies.length > 0 ? (
-                    <>
-                        <MoviesTable
-                            type='liked-list'
-                            moviesIds={likedMovies}
-                        />
-                        <MoviesMobileList
-                            type='liked-list'
-                            moviesIds={likedMovies}
-                        />
-                    </>
-                ) : (
-                    <div className={styles.likedList__wrapper}>
-                    <span className={styles.likedList__emptyWatchlist}>
-                        You haven't yet added any movie to your liked list.
-                    </span>
-                        &nbsp;
-                        <Link
-                            to='/movies/top-rated'
-                            className={styles.likedList__exploreLink}
-                        >
-                            Start exploring!
-                        </Link>
-                    </div>
-                )}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Helmet>
+        <title>{userName} - Liked list &bull; Recommend.me</title>
+      </Helmet>
+      <div className={styles.likedList}>
+        <Title title="Movies You liked" />
+        {likedMovies.length > 0 ? (
+          <>
+            <MoviesTable type="liked-list" moviesIds={likedMovies} />
+            <MoviesMobileList type="liked-list" moviesIds={likedMovies} />
+          </>
+        ) : (
+          <div className={styles.likedList__wrapper}>
+            <span className={styles.likedList__emptyWatchlist}>
+              You haven't yet added any movie to your liked list.
+            </span>
+            &nbsp;
+            <Link
+              to="/movies/top-rated"
+              className={styles.likedList__exploreLink}
+            >
+              Start exploring!
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default LikedList;
--- src/pages/Profile/WatchList/index.js
+++ src/pages/Profile/WatchList/index.js
@@ -1,54 +1,48 @@
 import React from 'react';
 
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { Helmet } from "react-helmet";
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Helmet } from 'react-helmet';
 
-import Title from "../MoviesTable/Title";
-import MoviesTable from "../MoviesTable";
-import MoviesMobileList from "../MoviesMobileList";
+import Title from '../MoviesTable/Title';
+import MoviesTable from '../MoviesTable';
+import MoviesMobileList from '../MoviesMobileList';
 import * as UserSelectors from '../../../store/shared/user/User.selectors';
 import styles from './WatchList.module.scss';
 
 const WatchList = () => {
-    const userName = useSelector(UserSelectors.name);
-    const watchlist = useSelector(UserSelectors.watchlist);
+  const userName = useSelector(UserSelectors.name);
+  const watchlist = useSelector(UserSelectors.watchlist);
 
-    return (
-        <>
-            <Helmet>
-                <title>{userName} - Watchlist &bull; Recommend.me</title>
-            </Helmet>
-            <div className={styles.watchlist}>
-                <Title title='Your watchlist'/>
-                {watchlist.length > 0 ? (
-                    <>
-                        <MoviesTable
-                            type='watch-list'
-                            moviesIds={watchlist}
-                        />
-                        <MoviesMobileList
-                            type='watch-list'
-                            moviesIds={watchlist}
-                        />
-                    </>
-                ) : (
-                    <div className={styles.watchlist__wrapper}>
-                    <span className={styles.watchlist__emptyWatchlist}>
-                        You haven't yet added any movie to your watchlist.
-                    </span>
-                        &nbsp;
-                        <Link
-                            to='/movies/top-rated'
-                            className={styles.watchlist__exploreLink}
-                        >
-                            Start exploring!
-                        </Link>
-                    </div>
-                )}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Helmet>
+        <title>{userName} - Watchlist &bull; Recommend.me</title>
+      </Helmet>
+      <div className={styles.watchlist}>
+        <Title title="Your watchlist" />
+        {watchlist.length > 0 ? (
+          <>
+            <MoviesTable type="watch-list" moviesIds={watchlist} />
+            <MoviesMobileList type="watch-list" moviesIds={watchlist} />
+          </>
+        ) : (
+          <div className={styles.watchlist__wrapper}>
+            <span className={styles.watchlist__emptyWatchlist}>
+              You haven't yet added any movie to your watchlist.
+            </span>
+            &nbsp;
+            <Link
+              to="/movies/top-rated"
+              className={styles.watchlist__exploreLink}
+            >
+              Start exploring!
+            </Link>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default WatchList;
